1e5f3b2b2a91618e45b63b03efd03f1fc841ba4c
kbakaras <kbakaras@gmail.com>
2019-07-30 22:51:09 +0300

    Команда resume позволяет указывать очередь.

--- src/main/java/ru/kbakaras/e2/cli/ResumeCommand.java
+++ src/main/java/ru/kbakaras/e2/cli/ResumeCommand.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import picocli.CommandLine;
 import picocli.CommandLine.ParentCommand;
+import ru.kbakaras.e2.cli.support.E2Queue;
 
 import java.util.Map;
 import java.util.concurrent.Callable;
@@ -18,12 +19,15 @@ public class ResumeCommand implements Callable<Void> {
     @ParentCommand
     private E2Command parent;
 
+    @CommandLine.Option(names = { "-q", "--queue"}, required = true, description = "Запрашиваемая очередь")
+    private E2Queue queue;
+
     private ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public Void call() throws Exception {
         ObjectNode request = mapper.createObjectNode();
-        request.put("queue", "delivery"); //TODO Create parameter
+        request.put("queue", queue.name());
 
         Map<String, Object> result = parent.createConnector().sendPost(
                 "Queue/resume",
